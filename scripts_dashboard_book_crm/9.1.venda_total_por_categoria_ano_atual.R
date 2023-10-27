44f45d765f4c73a2999a6ea066e18fcba401b35f
Guilherme Souza <guilherme.agsouza@hotmail.com>
2023-10-27 11:02:29 -0300

    Exclusão de algumas variáveis inutilizadas no script de vendas e atualização sem mudança relevante no script de estoque.

--- scripts_dashboard_book_crm/9.1.venda_total_por_categoria_ano_atual.R
+++ scripts_dashboard_book_crm/9.1.venda_total_por_categoria_ano_atual.R
@@ -42,7 +42,8 @@ from [sgm].[dbo].[secao]"
 #### 1.5 JUNTA AS BASES DOS SKUS POR CATEGORIA ####
 df_produtos %<>% 
   dplyr::left_join(df_categorias, by = c('SECAO','GRUPO','SUBGRUPO')) %>% 
-  dplyr::select(COD_INTERNO, DESCRICAO_COMPLETA, NOME_SECAO, NOME_GRUPO, NOME_SUBGRUPO, SITUACAO)
+  dplyr::select(COD_INTERNO, DESCRICAO_COMPLETA, NOME_SECAO, NOME_GRUPO, NOME_SUBGRUPO, SITUACAO) %>% 
+  dplyr::mutate_if(is.character, str_trim)
 #########
 
 data_de_hoje <- lubridate::today()
@@ -53,44 +54,84 @@ df_venda_loja <- DBI::dbGetQuery(
   statement = paste0("SELECT
   v.LOJA,
   100*YEAR(DATA) + MONTH(DATA) AS ANO_MES,
+  MONTH(DATA) AS MES,
 v.COD_INTERNO,
 SUM(QUANTIDADE) AS QUANTIDADE_TOTAL,
-(sum(PRECO_TOTAL)-ISNULL(SUM(DESCONTO), 0)) AS PRECO_TOTAL,
-SUM(PRECO_TOTAL)/SUM(QUANTIDADE) AS PRECO_MEDIO
+(sum(PRECO_TOTAL)-ISNULL(SUM(DESCONTO), 0)) AS PRECO_TOTAL
 FROM VM_INTEGRACAO.dbo.vw_propz v
 LEFT JOIN VM_DATABSP.dbo.PRODUTOS p ON v.COD_INTERNO = p.PRODUTO_ID
 WHERE DATA BETWEEN '2023-01-01' AND '",data_de_hoje,"'","
-GROUP BY v.LOJA,100*YEAR(DATA) + MONTH(DATA), v.COD_INTERNO, p.DESCRICAO_COMPLETA
+GROUP BY v.LOJA,100*YEAR(DATA) + MONTH(DATA), MONTH(DATA), v.COD_INTERNO, p.DESCRICAO_COMPLETA
 ORDER BY SUM(PRECO_TOTAL) DESC"
 )
 )
 
+# Tira os espaços das strings
+df_venda_loja %<>% dplyr::mutate_if(is.character, str_trim)
+
 #### 2.1 JUNTA BASE DE VENDA COM CATEGORIAS DOS SKUS ####
 df_venda_consolidada <- df_venda_loja %>% 
   dplyr::left_join(df_produtos, by = c('COD_INTERNO')) %>% 
-  dplyr::left_join(descricao_loja, by = c('LOJA'))
+  dplyr::left_join(descricao_loja, by = c('LOJA')) %>% 
+  dplyr::mutate_if(is.character, str_trim) 
 
-df_mes_nome <- data.frame(
-  ANO_MES = seq(202301,202312),
-  MES = c("Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", 
-          "Ago", "Set", "Out", "Nov", "Dez")
-)
+#df_mes_nome <- data.frame(
+#  ANO_MES = seq(202301,202312),
+#  MES = c("Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", 
+#          "Ago", "Set", "Out", "Nov", "Dez")
+#)
 
 # Altera os nomes das situações dos skus
 df_venda_consolidada %<>% 
-  dplyr::left_join(df_mes_nome, by = c('ANO_MES')) %>% 
+  #dplyr::left_join(df_mes_nome, by = c('ANO_MES')) %>% 
   dplyr::mutate(
     SITUACAO = case_when(
       SITUACAO == "A" ~ "Ativo",
       SITUACAO == "E" ~ "Excluído",
       SITUACAO == "D" ~ "Descontinuado",
       TRUE ~ NA_character_  # Caso contrário, definir como NA
-  ),
-  PROP_PRECO_TOTAL = PRECO_TOTAL/sum(PRECO_TOTAL)
   )
+  ) %>%
+  dplyr::mutate_if(is.character, str_trim) 
+
+###############
+# Seleciona somente CÓDIGO SKU E DESCRIÇÃO COMPLETA
+#df_codsku_descricaocompleta <- df_venda_consolidada %>% 
+#  dplyr::select(COD_INTERNO, DESCRICAO_COMPLETA, PRECO_TOTAL) %>% 
+#  dplyr::group_by(COD_INTERNO, DESCRICAO_COMPLETA) %>% 
+#  dplyr::summarise(PRECO_TOTAL = sum(PRECO_TOTAL, na.rm = TRUE)) %>% 
+#  dplyr::arrange(desc(DESCRICAO_COMPLETA)) %>% 
+#  ungroup() %>% 
+#  dplyr::group_by(DESCRICAO_COMPLETA) %>%
+#  dplyr::slice_max(PRECO_TOTAL) %>% 
+#  dplyr::ungroup() %>% 
+#  dplyr::mutate_if(is.character, str_trim) %>% 
+#  dplyr::select(-PRECO_TOTAL) %>% 
+#  dplyr::left_join(
+#    df_produtos %>% 
+#      dplyr::select(COD_INTERNO, DESCRICAO_COMPLETA,SITUACAO), 
+#    by = c('COD_INTERNO','DESCRICAO_COMPLETA')
+#  )
+
+#df_venda_consolidada %<>%
+#  # Apagando os códigos internos diferentes e mantendo apenas o principal
+#  dplyr::select(-COD_INTERNO,-SITUACAO) %>% 
+#  dplyr::mutate_if(is.character, str_trim) %>% 
+#  dplyr::left_join(df_codsku_descricaocompleta, by = c('DESCRICAO_COMPLETA')) %>% 
+#  dplyr::select(-LOJA)
+#  #dplyr::group_by(across(c(-QUANTIDADE,)))
+############
 
 df_venda_consolidada %>% 
   write.csv2("output_dashboard_book_crm/11.venda_total_ano_atual.csv", sep = ",",dec = ",", row.names = FALSE)
+
+atualizacao_venda_dash1 <- data.frame(
+  DATA_ATUALIZACAO_VENDA_GERAL = now() %>% lubridate::ymd_hms()
+)
+writexl::write_xlsx(
+  list(data_atualizacao_dash_de_venda = atualizacao_venda_dash1),
+  "output_dashboard_book_crm/11.2.data_atualizacao_venda_dash1.xlsx"
+)
 
 # Final do processo
 end.time <- Sys.time()
